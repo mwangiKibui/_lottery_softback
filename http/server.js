70f5a2fd49e6bfd4bf46cce364ce0d6db703acee
kennedy mwangi <kennedy.mwangi@equitybank.co.ke>
2025-05-01 23:36:29 +0300

    added https config

--- http/server.js
+++ http/server.js
@@ -1,11 +1,24 @@
 // http/server.js
 const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const app = require("../app/app");
 
 const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 8080;
+
+// ssl configuration.
+const options = {
+  key: fs.existsSync('/etc/letsencrypt/live/lotterysoft.net/privkey.pem') ? fs.readFileSync('/etc/letsencrypt/live/lotterysoft.net/privkey.pem') : null,
+  cert: fs.existsSync('/etc/letsencrypt/live/lotterysoft.net/fullchain.pem') ? fs.existsSync('/etc/letsencrypt/live/lotterysoft.net/fullchain.pem') : null
+};
+
 httpServer.listen(PORT, () => {
   console.log(`HTTP server is running on port ${PORT}.`);
+});
+
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS Server running on port 443');
 });
 
 module.exports = httpServer;
